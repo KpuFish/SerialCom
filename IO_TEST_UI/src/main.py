33611e3b324dabd3c1cd27e7e19ca5557c87884e
FISH\Fish <KpuFish>
2023-11-22 21:19:32 +0900

    23.11.22  - SerialCom update V0.1

--- IO_TEST_UI/src/main.py
+++ IO_TEST_UI/src/main.py
@@ -8,7 +8,7 @@ from main_ui import Ui_MainWindow
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 from PySide6.QtSerialPort import QSerialPort, QSerialPortInfo
-from PySide6.QtCore import QFile, QThread
+from PySide6.QtCore import QFile, QThread, QMutex, Signal, Slot
 
 import time
 
@@ -46,30 +46,24 @@ class MainWindow(QMainWindow) :
         self.ui.send.clicked.connect(self.send_serial)
         self.ui.pushButton_clear.clicked.connect(self.rxclear)
         self.ui.pushButton_scan.clicked.connect(self.scan)
-        #self.ui.rxline.textChanged.connect(self.receive_serial)
+        
         # rx thread set
         self.rxThread = ReceiveSerial(self)
+        #self.rxThread.rx_signal.connect(self.receive_serial)
         #self.rxThread.daemon = True
 
     # my serial open methode
     def open_port(self) :
         port_name = self.ui.current_comport.currentText()
         baudrate = int(self.ui.current_baudrate.currentText())
+
+        self.serial_port.setPortName(port_name)
+        self.serial_port.setBaudRate(baudrate)
+        self.serial_port.setFlowControl(QSerialPort.FlowControl.NoFlowControl)
+        self.serial_port.setDataBits(QSerialPort.DataBits.Data8)
+        self.serial_port.setParity(QSerialPort.Parity.NoParity)
+        self.serial_port.setStopBits(QSerialPort.StopBits.OneStop)
         
-        if False :
-            self.serial_port.setPortName(port_name)
-            self.serial_port.setBaudRate(baudrate, self.serial_port.Direction.AllDirections)
-            self.serial_port.setFlowControl(self.serial_port.FlowControl.NoFlowControl)
-            self.serial_port.setDataBits(self.serial_port.DataBits.Data8)
-            self.serial_port.setParity(self.serial_port.Parity.NoParity)
-            self.serial_port.setStopBits(self.serial_port.StopBits.OneStop)
-        else :
-            self.serial_port.setPortName(port_name)
-            self.serial_port.setBaudRate(baudrate) #, QSerialPort.Direction.AllDirections)
-            self.serial_port.setFlowControl(QSerialPort.FlowControl.NoFlowControl)
-            self.serial_port.setDataBits(QSerialPort.DataBits.Data8)
-            self.serial_port.setParity(QSerialPort.Parity.NoParity)
-            self.serial_port.setStopBits(QSerialPort.StopBits.OneStop)
         #-------------------------------------------------------------
         # DBG Prin
         #-------------------------------------------------------------
@@ -81,10 +75,8 @@ class MainWindow(QMainWindow) :
         #-------------------------------------------------------------
         
         try :
-            #if self.serial_port.open(self.serial_port.OpenModeFlag.ReadWrite) : 
             if not self.serial_port.isOpen() :
                 self.serial_port.open(QSerialPort.OpenModeFlag.ReadWrite)
-                #self.serial_port.Serial(port_name, baudrate)
                 self.statusBar().showMessage(f"OPENED")
                 self.ui.pushButton_open.setDisabled(True)
                 self.ui.pushButton_close.setEnabled(True)
@@ -106,6 +98,8 @@ class MainWindow(QMainWindow) :
             self.statusBar().showMessage("CLOSED")
             self.ui.pushButton_open.setEnabled(True)
             self.ui.pushButton_close.setDisabled(True)
+            
+            # Serial Rx Thread Stop
             self.rxThread.is_running = False
             self.rxThread.quit()
         else :
@@ -119,21 +113,21 @@ class MainWindow(QMainWindow) :
             tx_data = tx_data + "\n"
             
             try :
-                self.serial_port.write(tx_data.encode('ascii')) # send tx data to connected serial port
+                if self.serial_port.isWritable() :
+                    self.serial_port.write(tx_data.encode('ascii')) # send tx data to connected serial port
             except Exception as error :
                 print(error)
             
             tx = "\nTX >> " + tx_data
-            self.ui.rxline.append(tx)
-            #self.ui.rxline.insertPlainText(tx)
+            #self.ui.rxline.append(tx)
+            self.ui.rxline.insertPlainText(tx)
             #self.ui.rxline.insertPlainText(str(tx, 'utf-8', 'replace')) # print tx string to rx text line edit
             #print(tx.encode('utf-8')) # print console to see the sent data
 
+    #@Slot(str)
     #def receive_serial(self):
-    #    # Serial Rx Thread Start
-    #    self.rxThread.is_running = True
-    #    self.rxThread.start()
-    #    #self.ui.rxline.insertPlainText(str(rx_data, 'utf-8', 'replace'))
+    #    rx_data = self.serial_port.readLine()
+    #    self.ui.rxline.insertPlainText(str(rx_data, 'utf-8', 'replace'))
 
     # serial rx text all clear
     def rxclear(self) :
@@ -145,42 +139,54 @@ class MainWindow(QMainWindow) :
         port_list = QSerialPortInfo().availablePorts()
         for port_index in port_list:
             self.ui.current_comport.addItem(port_index.portName()) 
-            
+    
 
-
+#----------------------------------------------------------------
+# RX Thread for serial parsing
+#----------------------------------------------------------------
 class ReceiveSerial(QThread) :
-    #def __init__(self, parent=None) :
-    def __init__(self, parent=None) :
+    rx_signal = Signal(str)
+    
+    def __init__(self, parent = None) :
         super(ReceiveSerial, self).__init__(parent)
         self.serial_monitor = parent
         self.is_running = False
-
+        
     def __del__(self) :
         self.is_running = False
-        self.rxThread.stop()
+        self.serial_monitor.rxThread.stop()
     
     # QThread for RX Serial Start... from xxx.start() method
     def run(self) :
-        while self.is_running :
+        rx_data = ""
+        while self.is_running :            
             if self.serial_monitor.serial_port.isReadable() :
                 rx_data = self.serial_monitor.serial_port.readLine()
-                if  rx_data :
-                    self.serial_monitor.ui.rxline.insertPlainText(str(rx_data, 'utf-8', 'replace'))
-                    #self.serial_monitor.ui.rxline.append(str(rx_data))
-                    #print(str(rx_data).encode('utf-8'))
-                                
-            self.msleep(1)
+            if  rx_data :
+                #self.rx_signal.emit("") # Signal - Slot
+                self.serial_monitor.ui.rxline.insertPlainText(str(rx_data, 'utf-8', 'replace'))
+                
+            self.msleep(10)
 
+
+
+
+#----------------------------------------------------------------
+# Exception Hook override
+#----------------------------------------------------------------
+#def ExceptionHook(exctype, value, traceback):
+#    sys.__excepthook__(exctype, value, traceback)
+#    #sys.exit(1) # print error with app exit
     
-    
-    
-    
- #----------------------
- # System Main Start...
- #----------------------
+
+
+#----------------------------------------------------------------
+# System Main Start...
+#----------------------------------------------------------------
 if __name__ == "__main__" :
+#    sys.excepthook = ExceptionHook # 예외 후크 설정 부분
+    
     app = QApplication(sys.argv)
-
     window = MainWindow()
     window.show()
 
--- IO_TEST_UI/src/myserial.py
+++ IO_TEST_UI/src/myserial.py
@@ -5,9 +5,6 @@ from PySide6.QtSerialPort import QSerialPort, QSerialPortInfo
 from PySide6.QtCore import QFile
 from main_ui import Ui_MainWindow
 
-
-
-"""
  # 시리얼포트 상수 값
     BAUDRATES = (
         QSerialPort.BaudRate.Baud1200,
@@ -50,52 +47,5 @@ from main_ui import Ui_MainWindow
 """
 
 
-#class MySerialPort(Ui_MainWindow) :
-class MySerialPort(Ui_MainWindow) :
-    def __init__(self, parent = None) :
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        
-        super(MySerialPort, self).__init__(parent)
-        self.serial_port = QSerialPort(self)
-        
-        # close btn disable
-        self.ui.pushButton_close.setDisabled(True)
-        
-        # uart baudrate list
-        baudrate = ["9600", "38400", "115200", "921600"]
-        self.ui.current_baudrate.addItems(baudrate)
-        
-        # uart comport list
-        port_list = QSerialPortInfo().availablePorts()
-        for port_index in port_list:
-            self.ui.current_comport.addItem(port_index.portName()) 
- 
-        self.ui.pushButton_open.clicked.connect(self.open_port)
-        self.ui.pushButton_close.clicked.connect(self.close_port)
-    
-    def open_port(self) :
-        port_name = self.ui.current_comport.currentText()
-        self.serial_port.setPortName(port_name)
-        
-        #if self.serial_port.isOpen() : 
-        if self.serial_port.open(QSerialPort.ReadWrite) :
-            self.statusBar().showMessage(f"OPENED")
-            self.ui.pushButton_open.setDisabled(True)
-            self.ui.pushButton_close.setEnabled(True)
-        else :
-            self.statusBar().showMessage(f"Failed to Open")
-            self.ui.pushButton_open.setEnabled(True)
-            self.ui.pushButton_close.setDisabled(True)
-            
-    def close_port(self) :
-        if self.serial_port.isOpen() :
-            self.serial_port.close()
-            self.statusBar().showMessage("CLOSED")
-            self.ui.pushButton_open.setEnabled(True)
-            self.ui.pushButton_close.setDisabled(True)
-        else :
-            self.ui.pushButton_open.setDisabled(True)
-            self.ui.pushButton_close.setEnabled(True)
 
-"""
+
